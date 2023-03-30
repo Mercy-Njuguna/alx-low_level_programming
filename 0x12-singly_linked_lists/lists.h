34fa39055fa03700a4b459136cd86e172daea711
Mercy Codes <mercynjuguna751@gmail.com>
2023-03-30 12:29:38 -0700

    my header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,12 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LISTS_H
+#define LISTS_H
 
-#include <stdio.h>
+/*
+* Desc: header file containinf prototypes and definitions
+*	for all functions and types written in this proj
+*/
+
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,6 +15,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *              for Holberton project
  */
 typedef struct list_s
 {
@@ -26,4 +30,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H */
