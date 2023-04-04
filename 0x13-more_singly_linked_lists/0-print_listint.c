f664b98cfa12ae06d1e76b010d603f932fd54f66
Mercy Codes <mercynjuguna751@gmail.com>
2023-04-04 05:18:01 -0700

    MC

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: head of the list
- *
- * Return: the number of nodes
+ * print_listint - print integer in list
+ * @h: struct listint_t
+ * Return:value of size_t
  */
+
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *cursor = h;
-	size_t count = 0;
+	size_t counter = 0;
 
-	while (cursor != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", cursor->n);
-		count += 1;
-		cursor = cursor->next;
+		printf("%d\n", h->n);
+		counter++;
+		h = h->next;
 	}
-	return (count);
-}
 
+	return (counter);
+}
