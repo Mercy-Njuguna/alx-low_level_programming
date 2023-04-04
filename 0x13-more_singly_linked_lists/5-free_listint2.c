422865c65a1e4f5de8cadf00623f8a96d6ded9ef
Mercy Codes <mercynjuguna751@gmail.com>
2023-04-04 10:59:28 -0700

    correction

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,12 +13,16 @@ void free_listint2(listint_t **head)
 
 	if (head != NULL)
 	{
-	
+		/*set head addr to current*/
 		current = *head;
 
+		/*iterate through the whole list*/
+		/*while setting the current node to temp*/
 		while ((temp = current) != NULL)
 		{
+			/*set next node to curretnt*/
 			current = current->next;
+			/*free temp, that is the current node*/
 			free(temp);
 		}
 
