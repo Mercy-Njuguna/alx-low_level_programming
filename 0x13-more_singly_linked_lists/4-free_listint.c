18775a7f744ee317b0b50c01bdc0a0fb12ae07f0
Mercy Codes <mercynjuguna751@gmail.com>
2023-04-04 10:47:57 -0700

    correction

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * free_listint - frees list
- * @head: struct head
+ * free_listint - free list
  *
+ * @head: pointer to the first node of the list
+ *
+ * Return: nothing
  */
-
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+	listint_t *current;
 
-
-	while (head != NULL)
+	/*iterate through each node in list*/
+	while ((current = head) != NULL)
 	{
-		tmp = head;
-		free(head);
-		head = tmp->next;
+		/*set head as the next node*/
+		head = head->next;
+		free(current);
 	}
-
 }
