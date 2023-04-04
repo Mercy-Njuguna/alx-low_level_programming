d9c8d610c2719dd643eaa86ef8220de9783221b1
Mercy Codes <mercynjuguna751@gmail.com>
2023-04-04 10:40:56 -0700

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees list
+ * @head: struct head
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+
+	while (head_n != NULL)
+	{
+		tmp = head_n;
+		free(head);
+		head_n = tmp->next;
+	}
+
+}
