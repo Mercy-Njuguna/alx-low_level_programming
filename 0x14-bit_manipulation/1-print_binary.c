dc93faee7b1d68c7b8f1eaf96a1387bfe35e0b8c
Mercy Codes <mercynjuguna751@gmail.com>
2023-04-08 04:25:32 -0700

    Task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
+ * print_binary - prints the binary representation of an unsigned long int
+ *
+ * @n: number to print in binary
+ *
+ * Return: void
  */
-void print_binary(unsigned long int n)
+
+void print_binary(unsigned long int x)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+	if (x > 1)
+	{
+		print_binary(x >> 1);
+	}
 
-	_putchar((n & 1) + '0');
+	_putchar((x & 1) + '0');
 }
-
 
