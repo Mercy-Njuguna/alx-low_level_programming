b875c39f3476d5d7399804f395c0ea02bbaddad1
Mercy Codes <mercynjuguna751@gmail.com>
2023-04-08 03:44:29 -0700

    a function that converts a binary number to an unsigned int.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ *
+ * @b: a pointer to a string of 0 and 1 chars
+ *
+ * Return: the converted number or 0 if invalid input
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+
+	for (; *b != '\0'; ++b)
+	{
+		if (*b != '0' && *b != '1')
+		{
+			return (0);
+		}
+
+		num = (num << 1) | (*b - '0');
+	}
+
+	return (num);
+}
+
