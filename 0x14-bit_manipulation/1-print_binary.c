b45c1199c160d07f78231db6eb2544a0cbfc1939
Mercy Sparkle <122865038+Mercy-Njuguna@users.noreply.github.com>
2023-04-08 14:29:06 +0300

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,9 +2,7 @@
 
 /**
  * print_binary - prints the binary representation of an unsigned long int
- *
  * @n: number to print in binary
- *
  * Return: void
  */
 void print_binary(unsigned long int x)
